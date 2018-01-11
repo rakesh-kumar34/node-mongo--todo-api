980e6ab91eaa72129fcddde07d81dcea690b536a
Rakesh Kumar <rakeshgat@gmail.com>
2018-01-11 00:33:30 -0600

    First Commit

---
+++ server/server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+const mongoose = require('./db/mongoose');
+let {Todo} = require('./models/todo');
+let {User} = require('./models/user');
+
+var app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+//POST from Client
+app.post('/todos', (req, res) => {
+    let newtodo = new Todo({
+        task: req.body.task
+    });
+
+    newtodo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+//GET All Todos
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send(todos);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+//GET indivdual todo
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(404).send();
+    });
+});
+
+//DELETE a Todo
+app.delete('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(404).send();
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
+
+
